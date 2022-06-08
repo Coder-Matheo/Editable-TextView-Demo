5fe914b5e0757d09b899f506bc5c295f508cd25a
Coder-Matheo <ali.dinarvand1370@gmail.com>
2022-06-08 16:26:35 +0200

    Editable TextView Demo

--- app/src/main/java/com/example/editabletextviewdemo/MainActivity.java
+++ app/src/main/java/com/example/editabletextviewdemo/MainActivity.java
@@ -5,41 +5,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    AlertDialog alertDialog;
-    EditText editText;
+    TextView emailAccountTextView;
+    TextView passwordAccountTextView;
+    TextView numberAccountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
-        alertDialog = new AlertDialog.Builder(this).create();
-        editText = new EditText(this);
 
-        alertDialog.setTitle("Edit the Text");
-        alertDialog.setView(editText);
+        setInitElement();
+    }
+    private void setInitElement() {
+        passwordAccountTextView = findViewById(R.id.passwordAccountTextView);
+        emailAccountTextView = findViewById(R.id.emailAccountTextView);
+        numberAccountTextView = findViewById(R.id.numberAccountTextView);
 
-        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "SAVE TEXT", new DialogInterface.OnClickListener() {
+        View.OnClickListener handler = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.emailAccountTextView:
+                        setEditableTextView(emailAccountTextView, "E-Mail");
+                        break;
+                    case R.id.passwordAccountTextView:
+                        setEditableTextView( passwordAccountTextView, "Password");
+                        break;
+                    case R.id.numberAccountTextView:
+                        setEditableTextView( numberAccountTextView, "Phone Number");
+                        break;
+                }
+            }
+        };
+
+        passwordAccountTextView.setOnClickListener(handler);
+        emailAccountTextView.setOnClickListener(handler);
+        numberAccountTextView.setOnClickListener(handler);
+    }
+    private void setEditableTextView(TextView emailAccountTextView, String titleDialog) {
+
+        AlertDialog dialog = new AlertDialog.Builder(this).create();
+        EditText emailEditText = new EditText(this);
+        emailEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        dialog.setTitle("Edit " + titleDialog);
+        dialog.setView(emailEditText);
+
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Save ", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                textView.setText(editText.getText());
+                emailAccountTextView.setText(emailEditText.getText());
             }
         });
 
-        textView.setOnClickListener(new View.OnClickListener() {
+        emailAccountTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(textView.getText());
-                alertDialog.show();
+                emailEditText.setText(emailAccountTextView.getText());
+                dialog.show();
             }
         });
-
     }
+
 }
